a65a8369ab07a94c0f2579c75b1e5038188aebc0
Steven Ghyselbrecht <steven.ghyselbrecht@gmail.com>
2010-11-13 17:19:39 +0100

    Add SSL support for github

--- jquery.rb
+++ jquery.rb
@@ -15,7 +15,7 @@ end
 get "http://code.jquery.com/jquery-latest.min.js", "public/javascripts/jquery.js"
 
 # Downloading latest jQuery drivers
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+get "https://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
 # Overriding default expansion
 if yes?("Override :defaults and setup :jquery expansion?")
@@ -38,7 +38,7 @@ elsif yes?("Override :defaults only?")
 	# Switch the javascript_include_tag :defaults to
 	# use jQuery instead of the default prototype helpers.
 	# Written by: Logan Leger, logan@loganleger.com
-	# http://github.com/lleger/Rails-3-jQuery
+	# https://github.com/lleger/Rails-3-jQuery
 
 	ActiveSupport.on_load(:action_view) do
 	  ActiveSupport.on_load(:after_initialize) do
@@ -50,7 +50,7 @@ elsif yes?("Setup :jquery expansion only?")
 	initializer 'jquery.rb', <<-CODE
 	# Setup a :jquery expansion, just for good measure.
 	# Written by: Logan Leger, logan@loganleger.com
-	# http://github.com/lleger/Rails-3-jQuery
+	# https://github.com/lleger/Rails-3-jQuery
 
 	ActionView::Helpers::AssetTagHelper.register_javascript_expansion :jquery => ['jquery', 'rails']
 	CODE
