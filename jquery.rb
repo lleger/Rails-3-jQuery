3bba8164785c70e5e25ed3c47098316391bde77d
Logan Leger <logan@loganleger.com>
2011-01-23 12:49:32 -0600

    Add rakefile and task to update js files

--- jquery.rb
+++ jquery.rb
@@ -28,3 +28,37 @@ application do
 	  config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 	  config.action_view.javascript_expansions[:cdn] = %w(https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js rails)\n"
 end
+
+# Add a jquery.rake file to lib/tasks
+# and a jquery:update task to update
+# jquery and rails drivers
+rakefile("jquery.rake") do
+  <<-TASK
+require 'net/http'
+require 'uri'
+
+namespace :jquery do
+  task :update do
+    http = Net::HTTP.new("code.jquery.com")
+    http.start do |http|
+        resp = http.get("/jquery-latest.min.js")
+        open("public/javascripts/jquery.js", "wb") do |file|
+            file.write(resp.body)
+        end
+    end
+
+    http = Net::HTTP.new("github.com", 443)
+    http.use_ssl = true
+    http.start do |http|
+        http.use_ssl = true
+        resp = http.get("/rails/jquery-ujs/raw/master/src/rails.js")
+        open("public/javascripts/rails.js", "wb") do |file|
+            file.write(resp.body)
+        end
+    end
+
+    puts "Updated jQuery and Rails jQuery drivers!"
+  end
+end
+  TASK
+end
